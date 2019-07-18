bd443cdd8d33b3b6593e9019350885ee921f05c6
stevephan98 <stevephan98@github.com>
2019-07-18 19:10:46 -0400

    added css and html styling to home page

--- jobs/views.py
+++ jobs/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.shortcuts import render
+from .models import Job
 
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'jobs/home.html')
+    jobs = Job.objects 
+    return render(request, 'jobs/home.html', {'jobs':jobs})
