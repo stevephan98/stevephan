7bf2f67d5e22816e27edc428e6ed57ef253ee200
stevephan98 <stevephan98@github.com>
2019-07-18 20:46:40 -0400

    added details to job page

--- jobs/views.py
+++ jobs/views.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 #from __future__ import unicode_literals
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Job
-
-from django.shortcuts import render
 
 def home(request):
     jobs = Job.objects 
     return render(request, 'jobs/home.html', {'jobs':jobs})
 
 def detail(request, job_id):
-    return render(request, 'jobs/home.html')
+    job_detail = get_object_or_404(Job, pk=job_id)
+    return render(request, 'jobs/detail.html', {'job':job_detail})
