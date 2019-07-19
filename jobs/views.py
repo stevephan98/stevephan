300134c446494e8e3e71857f883d0c7124becc99
stevephan98 <stevephan98@github.com>
2019-07-18 20:23:20 -0400

    added more jobs and urls for those jobs

--- jobs/views.py
+++ jobs/views.py
@@ -8,3 +8,6 @@ from django.shortcuts import render
 def home(request):
     jobs = Job.objects 
     return render(request, 'jobs/home.html', {'jobs':jobs})
+
+def detail(request, job_id):
+    return render(request, 'jobs/home.html')
